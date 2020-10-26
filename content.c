ceeae09e10c6fe6bec58664c31ed6b589461d956
Robert Hsu <roberthsu2003@gmail.com>
2020-10-26 15:44:43 +0800

    Update content.c

--- content.c
+++ content.c
@@ -6,10 +6,24 @@ void setup()
 
 void loop()
 {
+  buttonDetect();
+}
+
+void buttonDetect(){
   int sensorVal = digitalRead(2);
   if(sensorVal == 1){
     Serial.println("release");
+    ledStop();
   }else{
     Serial.println("press");
+    ledEffect();
   }
+}
+
+void ledEffect(){
+  Serial.println("do something of led");
+}
+
+void ledStop(){
+  Serial.println("led Stop");
 }
