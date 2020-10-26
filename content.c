7a97fd6f51f0505f616043a74048ba322bb4fe05
Robert Hsu <roberthsu2003@gmail.com>
2020-10-26 14:31:35 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,11 @@
 int buttonPin = 2;
-int buttonState = 0;  
+int buttonState = 0; 
+int ledPin = 13;
 void setup()
 {
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop()
@@ -11,7 +13,9 @@ void loop()
 	buttonState	= digitalRead(buttonPin);
   if(buttonState == 1){
     Serial.println("press");
+    digitalWrite(ledPin, HIGH);
   }else{
     Serial.println("release");
+    digitalWrite(ledPin, LOW);
   }
 }
