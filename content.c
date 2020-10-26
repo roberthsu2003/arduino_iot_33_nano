183ef6f555ef436defd9f1b83c1dec41ef0ef9cc
Robert Hsu <roberthsu2003@gmail.com>
2020-10-26 16:10:32 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,13 @@
+byte pins[] = {4, 5, 6, 7, 8};
+byte count = sizeof(pins)/sizeof(pins[0]);
+
 void setup()
 {
   Serial.begin(9600);
   pinMode(2,INPUT_PULLUP);
+  for(int i=0; i<count;i++){
+    pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop()
@@ -22,8 +28,16 @@ void buttonDetect(){
 
 void ledEffect(){
   Serial.println("do something of led");
+  for(int i=0; i<count;i++){
+    digitalWrite(pins[i],HIGH);
+    delay(1000);
+  }
+  ledStop();
 }
 
 void ledStop(){
   Serial.println("led Stop");
+  for(int i=0; i<count;i++){
+    digitalWrite(pins[i],LOW);
+  }
 }
