cd28d3a624c8e1860b8fcbcda56484404a290c32
Robert Hsu <roberthsu2003@gmail.com>
2020-10-27 14:54:29 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,40 @@
+#include "Firebase_Arduino_WiFiNINA.h"
+
+#define FIREBASE_HOST ""
+#define FIREBASE_AUTH ""
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+//Define Firebase data object
+FirebaseData firebaseData;
+
+void setup()
 {
-  "rules": {
-    ".read": true,
-    ".write": "auth.uid != null"
+
+  Serial.begin(9600);
+  delay(100);
+  Serial.println();
+
+  Serial.print("Connecting to Wi-Fi");
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED)
+  {
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print(".");
+    delay(300);
   }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  //Provide the autntication data
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH, WIFI_SSID, WIFI_PASSWORD);
+  Firebase.reconnectWiFi(true);
+  Serial.println("連線Firebase正常");
+  
+}
+
+void loop()
+{
 }
