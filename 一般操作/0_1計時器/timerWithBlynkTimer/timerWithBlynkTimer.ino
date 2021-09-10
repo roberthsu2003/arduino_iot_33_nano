baf99cf75c6e0efca16d0b45a293f3c2b4a69d17
roberthsu2003 <roberthsu2003@gmail.com>
2021-09-10 20:27:51 +0800

    modify

---
+++ 一般操作/0_1計時器/timerWithBlynkTimer/timerWithBlynkTimer.ino
@@ -0,0 +1,26 @@
+#include <BlynkSimpleWiFiNINA.h>
+#define ledPin  LED_BUILTIN
+BlynkTimer timer;
+bool ledState = LOW;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  timer.run();
+}
+
+void myTimerEvent()
+{
+  if (ledState == LOW) {
+    ledState = HIGH;
+  } else {
+    ledState = LOW;
+  }
+  digitalWrite(ledPin, ledState);
+}
