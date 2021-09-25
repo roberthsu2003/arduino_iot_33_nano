1caa8e1843cc835141cd76cadc4e39084d30eaa0
Robert Hsu <roberthsu2003@gmail.com>
2021-09-25 11:15:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,3 +1,4 @@
+char inputValue;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -8,7 +9,16 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()){
-    Serial.println(Serial.read());
+    inputValue = Serial.read();
+    
+    if(inputValue == '1'){
+      Serial.println("您輸入的是1");
+    }else if(inputValue == '0'){
+      Serial.println("您輸入的是0");
+    }else if(inputValue == '2'){
+      Serial.println("您輸入的是2");
+    }
+    
   }
  
 }
