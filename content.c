c78618cf76f3b0c39cf650e8b1aa358c84e675cd
Robert Hsu <roberthsu2003@gmail.com>
2021-09-25 11:42:09 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,7 +1,11 @@
+#define LED 13
+
 char inputValue;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+  
   while(!Serial);  
   Serial.println("Hello!Word!");
 }
@@ -13,10 +17,13 @@ void loop() {
     
     if(inputValue == '1'){
       Serial.println("您輸入的是1");
+      digitalWrite(LED,HIGH);
     }else if(inputValue == '0'){
       Serial.println("您輸入的是0");
+      digitalWrite(LED,LOW);
     }else if(inputValue == '2'){
       Serial.println("您輸入的是2");
+      digitalWrite(LED,HIGH);
     }
     
   }
