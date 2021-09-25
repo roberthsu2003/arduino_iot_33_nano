b32f510d0004a3518aa0d6fb459d4a6573e56de0
Robert Hsu <roberthsu2003@gmail.com>
2021-09-25 10:42:59 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  while(!Serial);
+  while(!Serial);  
   Serial.println("Hello!Word!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  if(Serial.available()){
+    Serial.println(Serial.read());
+  }
  
 }
