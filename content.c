9cd1dc713877e2c1918b2675614c2eeb83934027
Robert Hsu <roberthsu2003@gmail.com>
2021-09-25 11:59:49 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,31 +1,31 @@
-#define LED 13
+#define LED A2
 
 char inputValue;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED,OUTPUT);
-  
-  while(!Serial);  
+  pinMode(LED, OUTPUT);
+
+  while (!Serial);
   Serial.println("Hello!Word!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available()){
+  if (Serial.available()) {
     inputValue = Serial.read();
-    
-    if(inputValue == '1'){
+
+    if (inputValue == '1') {
       Serial.println("您輸入的是1");
-      digitalWrite(LED,HIGH);
-    }else if(inputValue == '0'){
+      digitalWrite(LED, HIGH);
+    } else if (inputValue == '0') {
       Serial.println("您輸入的是0");
-      digitalWrite(LED,LOW);
-    }else if(inputValue == '2'){
+      digitalWrite(LED, LOW);
+    } else if (inputValue == '2') {
       Serial.println("您輸入的是2");
-      digitalWrite(LED,HIGH);
+      digitalWrite(LED, HIGH);
     }
-    
+
   }
- 
+  analogWrite(LED,0);
 }
