edd37c9f56ee9fca811d1448a9d998c65a049abc
Robert Hsu <roberthsu2003@gmail.com>
2021-09-25 10:09:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,10 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Hello!Word!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello!Word!");
-  delay(1000);
+  
+ 
 }
