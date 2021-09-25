1f08425427e5cd201fae93772d804cd807f7377c
Robert Hsu <roberthsu2003@gmail.com>
2021-09-25 16:29:03 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,51 +1,20 @@
-#define RED 2
-#define GREEN 3
+#define BTN 2
+#define LED 13
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(RED,OUTPUT);
-  pinMode(GREEN, OUTPUT);
+  pinMode(BTN,INPUT);
+  pinMode(LED,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  intervalOne();
-  intervalTwo();
-}
-
-
-void intervalOne(){
-  static bool ledState = false;
-  static unsigned long previousTime = millis();
-  unsigned long currentTime;
-  currentTime = millis();
-  if(currentTime-previousTime >= 1000){
-    previousTime = currentTime;
-    Serial.println("每隔1秒執行一次");
-    ledState = !ledState;
-    if(ledState){
-      digitalWrite(RED,HIGH);
-    }else{
-      digitalWrite(RED,LOW);
-    }
-  }
-}
-
-void intervalTwo(){
-  static bool ledState = false;
-  static unsigned long previousTime = millis();
-  unsigned long currentTime;
-  currentTime = millis();
-  if(currentTime-previousTime >= 2000){
-    previousTime = currentTime;
-    Serial.println("每隔2秒執行一次");
-    ledState = !ledState;
-    if(ledState){
-      digitalWrite(GREEN,HIGH);
-    }else{
-      digitalWrite(GREEN,LOW);
-    }
+  bool sensorVal = digitalRead(BTN);
+  if(sensorVal){
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
   }
 }
