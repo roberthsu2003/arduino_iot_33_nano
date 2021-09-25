a9c6bc1ae2049c9bc174dbda96cddb4e39914315
Robert Hsu <roberthsu2003@gmail.com>
2021-09-25 15:13:41 +0800

    Update content.c

--- content.c
+++ content.c
@@ -9,7 +9,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   intervalOne();
-  
+  intervalTwo();
 }
 
 
@@ -20,5 +20,15 @@ void intervalOne(){
   if(currentTime-previousTime >= 1000){
     previousTime = currentTime;
     Serial.println("每隔1秒執行一次");
+  }
+}
+
+void intervalTwo(){
+  static unsigned long previousTime = millis();
+  unsigned long currentTime;
+  currentTime = millis();
+  if(currentTime-previousTime >= 2000){
+    previousTime = currentTime;
+    Serial.println("每隔2秒執行一次");
   }
 }
