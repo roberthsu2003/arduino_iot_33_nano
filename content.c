1f1d6f23876a01bbd3bbcee5673a822168a588cc
Robert Hsu <roberthsu2003@gmail.com>
2021-09-29 13:52:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -3,12 +3,16 @@
 void setup()
 {
   pinMode(4, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   digitalWrite(4, HIGH);
+  Serial.println("open");
   delay(1000); // Wait for 1000 millisecond(s)
+  
   digitalWrite(4, LOW);
+  Serial.println("close");
   delay(1000); // Wait for 1000 millisecond(s)
 }
