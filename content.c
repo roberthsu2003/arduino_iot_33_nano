150d84c9d63408795a8d465f5935b5b2e1fea632
Robert Hsu <roberthsu2003@gmail.com>
2021-09-29 16:02:10 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,34 @@
 // C++ code
 //
 
-unsigned long previousTime;
+
 void setup()
 {
   Serial.begin(9600);
-  previousTime = millis();
+  
 }
 
 void loop()
 {
 
  oneSecond();
- 
+ twoSecond();
 }
 
 void oneSecond(){
+  static unsigned long previousTime = millis();
   unsigned long currentTime = millis();
   if(currentTime - previousTime >=1000){
     previousTime = currentTime;
     Serial.println("over one Second");
+  }
+}
+
+void twoSecond(){
+  static unsigned long previousTime = millis();
+  unsigned long currentTime = millis();
+  if(currentTime - previousTime >=2000){
+    previousTime = currentTime;
+    Serial.println("over two Second");
   }
 }
