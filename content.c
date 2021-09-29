17f8e6ccd3836253fc0ce583d1f9d2c7a7bd3edc
Robert Hsu <roberthsu2003@gmail.com>
2021-09-29 16:17:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,11 +1,14 @@
 // C++ code
 //
 
+#define RED 4
+#define BLUE 5
 
 void setup()
 {
   Serial.begin(9600);
-  
+  pinMode(RED,OUTPUT);
+  pinMode(BLUE,OUTPUT);
 }
 
 void loop()
@@ -16,19 +19,25 @@ void loop()
 }
 
 void oneSecond(){
+  static bool redState = false;
   static unsigned long previousTime = millis();
   unsigned long currentTime = millis();
   if(currentTime - previousTime >=1000){
     previousTime = currentTime;
     Serial.println("over one Second");
+    redState = !redState;
+    digitalWrite(RED,redState);
   }
 }
 
 void twoSecond(){
+  static bool blueState = false;
   static unsigned long previousTime = millis();
   unsigned long currentTime = millis();
   if(currentTime - previousTime >=2000){
     previousTime = currentTime;
     Serial.println("over two Second");
+    blueState = !blueState;
+    digitalWrite(BLUE, blueState);
   }
 }
