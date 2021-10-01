8880329a9d8ce69d079b3d59f4339403232a4617
Robert Hsu <roberthsu2003@gmail.com>
2021-10-01 11:53:24 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,30 @@
+#include "password.h"
+
+#define BLYNK_PRINT Serial
+#include <SPI.h>
+#include <WiFiNINA.h>
 #include <BlynkSimpleWiFiNINA.h>
+
 #define VR A0
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = MY_PHONE_NAME;
+char pass[] = MY_PHONE_NUMBER;
 
 BlynkTimer timer;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
   timer.setInterval(100, myTimerEvent);
+  timer.setInterval(1000,sendVRValue);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- 
+ Blynk.run();
  timer.run();
 }
 
@@ -22,4 +34,11 @@ void myTimerEvent(){
 
 int vrValue(byte pin){
   return analogRead(pin);
+}
+
+
+void sendVRValue(){
+  short outputValue = vrValue(VR);
+  byte myValue = map(outputValue,0,1023,0,100);
+  Blynk.virtualWrite(V0, myValue);
 }
