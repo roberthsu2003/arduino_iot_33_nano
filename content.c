92c41a767fc8a81d576d82ad442eabdab6325bad
Robert Hsu <roberthsu2003@gmail.com>
2021-10-01 10:46:00 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,14 +1,23 @@
+#include <BlynkSimpleWiFiNINA.h>
 #define VR A0
+
+BlynkTimer timer;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  timer.setInterval(100, myTimerEvent);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- Serial.println(vrValue(VR));
- delay(10);
+ 
+ timer.run();
+}
+
+void myTimerEvent(){
+  Serial.println(vrValue(VR));
 }
 
 int vrValue(byte pin){
