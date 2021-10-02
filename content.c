438de81d109722995347a20da77a81c02fbc77c4
Robert Hsu <roberthsu2003@gmail.com>
2021-10-02 10:36:48 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,9 +1,12 @@
 #define BTN 2
+#define LED 13
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(BTN,INPUT_PULLUP);
+  pinMode(LED,OUTPUT);
+  
 }
 
 void loop() {
@@ -12,8 +15,10 @@ void loop() {
   
   if(btnState == LOW){
     Serial.println("按鈕被按下");
+    digitalWrite(LED,HIGH);
   }else{
     Serial.println("沒按按鈕");
+    digitalWrite(LED,LOW);
   }
   delay(10);
 }
