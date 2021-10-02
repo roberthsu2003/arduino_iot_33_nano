0e2a3c6359ec9a79fb553002d704e56d78bb8314
Robert Hsu <roberthsu2003@gmail.com>
2021-10-02 11:24:56 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,24 +1,12 @@
-#define BTN 2
-#define LED 13
+#define CDS A0
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(BTN,INPUT_PULLUP);
-  pinMode(LED,OUTPUT);
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  bool btnState = digitalRead(BTN);
-  
-  if(btnState == LOW){
-    Serial.println("按鈕被按下");
-    digitalWrite(LED,HIGH);
-  }else{
-    Serial.println("沒按按鈕");
-    digitalWrite(LED,LOW);
-  }
-  delay(10);
+  int sensorVal = analogRead(A0);
+  Serial.println(sensorVal);
+  delay(100);
 }
