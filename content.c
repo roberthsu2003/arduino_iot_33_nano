44a70fc47d6c4e7e6bb43d6ceb9fb008607e299d
Robert Hsu <roberthsu2003@gmail.com>
2021-10-02 14:23:23 +0800

    Update content.c

--- content.c
+++ content.c
@@ -5,10 +5,18 @@
 #define CDS A0
 char ssid[] = SSIDNAME;
 char pass[] = PASSWORD;
+int status = WL_IDLE_STATUS;
+char server[] = "www.google.com"; 
+WiFiClient client;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  while(!Serial);
+  if(WiFi.status() == WL_NO_MODULE){
+    Serial.println("沒有這個網路模組wifinina");
+  }
+  Serial.println("檢查完成");
 }
 
 void loop() {
@@ -20,17 +28,11 @@ int sensorVal = analogRead(A0);
   //Serial.println(sensorVal);
   
   if(sensorVal > 700){
-    Serial.println("暗");
+    //Serial.println("暗");
   }else if(sensorVal < 100){
-    Serial.println("亮");
+    //Serial.println("亮");
   }else{
-    Serial.println("正常");
+    //Serial.println("正常");
   }  
   delay(100);
 }
-
-
-secretData.h
-
-#define SSIDNAME ""
-#define PASSWORD ""
