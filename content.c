b5d98519f888a7eb3e478520f0bd7f92e0184164
Robert Hsu <roberthsu2003@gmail.com>
2021-10-02 10:03:17 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,47 +1,19 @@
-#include <SPI.h>
-#include <WiFiNINA.h>
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define BTN 2
 
-int status = WL_IDLE_STATUS;
-char server[] = "maker.ifttt.com";
-WiFiSSLClient client;
-
-
-void setup()
-{
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  delay(100);
-  Serial.println();
-
-  Serial.print("Connecting to Wi-Fi");
-  int status = WL_IDLE_STATUS;
-  while (status != WL_CONNECTED)
-  {
-    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-
-
-
-  //連線ifttt
-  //一開始就連線的測試
-    if (client.connect(server, 443)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("GET /trigger/over30/with/key/eDqcZfqY_i_BHCZVXCwb6aq7GLPKpdV4q1ePja35Mjq?value1=35&value2=55 HTTP/1.1");
-    client.println("Host: maker.ifttt.com");
-    client.println("Connection: close");
-    client.println();
-    }
-  
+  pinMode(BTN,INPUT);
 }
 
-void loop(){
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool btnState = digitalRead(BTN);
   
+  if(btnState == HIGH){
+    Serial.println("按鈕被按了");
+  }else{
+    Serial.println("沒按按鈕");
+  }
+  delay(10);
 }
