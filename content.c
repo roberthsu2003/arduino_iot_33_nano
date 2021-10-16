d723ebb1d6d074bdebb1400afb8b83240aa8a67e
Robert Hsu <roberthsu2003@gmail.com>
2021-10-16 09:52:38 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include "secret.h"
 
 char ssid[] = SSIDNAME; 
 char pass[] = PASSWORD;
@@ -7,18 +8,21 @@ char pass[] = PASSWORD;
 int status = WL_IDLE_STATUS;
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
   while(!Serial);
-  
+  //如果連線的狀態，不是WL_CONNECTED，就繼續執行
+  while(status != WL_CONNECTED){
+    Serial.print("連線到SSID");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(5000);
+  }
+
+  Serial.println("已經連線成功");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
-
-//secret.h
-#define SSIDNAME ""
-#define PASSWORD ""
-#define IFTTTKEY eDqPja35Mjq
