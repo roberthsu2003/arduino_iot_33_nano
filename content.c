a17fdef982154a72bb53bb8b03240a475fcc2170
Robert Hsu <roberthsu2003@gmail.com>
2021-10-16 16:26:16 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,3 +1,5 @@
+#include "data.h"
+
 #define INPUT_RED A0
 #define INPUT_GREEN A1
 #define INPUT_BLUE A2
@@ -6,34 +8,20 @@
 #define OUTPUT_BLUE 5
 
 
+RGB_LED r(INPUT_RED,OUTPUT_RED);
+RGB_LED g(INPUT_GREEN,OUTPUT_GREEN);
+RGB_LED b(INPUT_BLUE,OUTPUT_BLUE);
+
 void setup() {
-  Serial.begin(9600);
-  pinMode(OUTPUT_RED,OUTPUT);
-  pinMode(OUTPUT_GREEN,OUTPUT);
-  pinMode(OUTPUT_BLUE,OUTPUT);
+  Serial.begin(9600);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  readSensor();
+  r.run();
+  g.run();
+  b.run();
   delay(1000);
 }
 
-void readSensor(){
-  int r = analogRead(INPUT_RED);
-  int g = analogRead(INPUT_GREEN);
-  int b = analogRead(INPUT_BLUE);
-  byte rValue = map(r,0,1023,0,255);
-  byte gValue = map(g,0,1023,0,255);
-  byte bValue = map(b,0,1023,0,255);
-  Serial.println("紅色:" + String(rValue));
-  Serial.println("綠色:" + String(gValue));
-  Serial.println("藍色:" + String(bValue));
-  changColor(rValue,gValue,bValue); 
-}
 
-void changColor(byte r, byte g, byte b){
-  analogWrite(OUTPUT_RED,r);
-  analogWrite(OUTPUT_GREEN,g);
-  analogWrite(OUTPUT_BLUE,b);
-}
+
