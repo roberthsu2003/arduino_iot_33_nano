711fd28f4f2b24f636d6cea136b25976cbf97cfe
Robert Hsu <roberthsu2003@gmail.com>
2021-10-23 11:49:25 +0800

    Update content.c

--- content.c
+++ content.c
@@ -15,107 +15,16 @@ void setup() {
   pinMode(f,OUTPUT);
   pinMode(g,OUTPUT);
   Serial.begin(9600);
-  zero();
+  while(!Serial);  
+  displayNum();
 }
-
-void zero(){
-  digitalWrite(a,HIGH);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,HIGH);
-  digitalWrite(f,HIGH);
-  digitalWrite(g,LOW);
-}
-
-void one(){
-  digitalWrite(a,LOW);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,LOW);
-  digitalWrite(e,LOW);
-  digitalWrite(f,LOW);
-  digitalWrite(g,LOW);
-}
-
-void two(){
-  digitalWrite(a,HIGH);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,LOW);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,HIGH);
-  digitalWrite(f,LOW);
-  digitalWrite(g,HIGH);
-}
-
-void three(){
-  digitalWrite(a,HIGH);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,LOW);
-  digitalWrite(f,LOW);
-  digitalWrite(g,HIGH);
-}
-
-void four(){
-  digitalWrite(a,LOW);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,LOW);
-  digitalWrite(e,LOW);
-  digitalWrite(f,HIGH);
-  digitalWrite(g,HIGH);
-}
-
-void five(){
-  digitalWrite(a,HIGH);
-  digitalWrite(b,LOW);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,LOW);
-  digitalWrite(f,HIGH);
-  digitalWrite(g,HIGH);
-}
-
-void six(){
-  digitalWrite(a,HIGH);
-  digitalWrite(b,LOW);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,HIGH);
-  digitalWrite(f,HIGH);
-  digitalWrite(g,HIGH);
-}
-
-void seven(){
-  digitalWrite(a,HIGH);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,LOW);
-  digitalWrite(e,LOW);
-  digitalWrite(f,LOW);
-  digitalWrite(g,LOW);
-}
-
-void eight(){
-  digitalWrite(a,HIGH);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,HIGH);
-  digitalWrite(f,HIGH);
-  digitalWrite(g,HIGH);
-}
-
-void nine(){
-  digitalWrite(a,HIGH);
-  digitalWrite(b,HIGH);
-  digitalWrite(c,HIGH);
-  digitalWrite(d,HIGH);
-  digitalWrite(e,LOW);
-  digitalWrite(f,HIGH);
-  digitalWrite(g,HIGH);
+void displayNum(){
+  byte n = B01111110;//7段顯示器0的數字
+  for(int i=6; i>=0; i--){
+    Serial.print(bitRead(n,i));    
+  }
+  Serial.println();
+  
 }
 
 void loop() {
@@ -123,56 +32,55 @@ void loop() {
       switch(Serial.read()){
       case '0':
       Serial.println(0);
-      zero();
+      
       break;
 
       case '1':
       Serial.println(1);
-      one();
+      
       break;
 
       case '2':
       Serial.println(2);
-      two();
+      
       break;
 
       case '3':
       Serial.println(3);
-      three();
+      
       break;
 
       case '4':
       Serial.println(4);
-      four();
+      
       break;
 
       case '5':
       Serial.println(5);
-      five();
+      
       break;
 
       case '6':
       Serial.println(6);
-      six();
+      
       break;
 
       case '7':
       Serial.println(7);
-      seven();
+      
       break;
 
       case '8':
       Serial.println(8);
-      eight();
+      
       break;
 
       case '9':
       Serial.println(9);
-      nine();
+      
       break;
     }
   }
-
-  
+   
 
 }
