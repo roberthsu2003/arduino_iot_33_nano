6a7c31bf5e3c8457458a8fd26e9fb38a2b7c51ed
Robert Hsu <roberthsu2003@gmail.com>
2021-10-23 16:13:15 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,3 +1,15 @@
+secret.h
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_DEVICE_NAME ""
+#define BLYNK_AUTH_TOKEN ""
+
+#define ID ""
+#define PASS ""
+
+====================================
+main
+
+
 #define a 3
 #define b 4
 #define c 5
