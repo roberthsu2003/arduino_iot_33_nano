e636deed0eb7fd0c50c76dfbf31fc885e20ac9e7
Robert Hsu <roberthsu2003@gmail.com>
2021-10-30 14:24:30 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,33 +4,45 @@
 #define button 5
 unsigned long switchCount = 0; //狀態改變的次數
 int i = 0; //按下放開的次數
-bool buttonState;
+
 
 void setup() {
   Serial.begin(9600);
-  pinMode(button,INPUT_PULLUP);
-  buttonState = digitalRead(button);
+  pinMode(button, INPUT_PULLUP);
+  
 }
 
-void loop() {
+int button_release() {
+  static bool buttonState = digitalRead(button);
   bool currentButtonState = digitalRead(button);
-  if(currentButtonState != buttonState){
+  if (currentButtonState != buttonState) {
     delay(30); //解決彈跳
-    if(currentButtonState != buttonState){
+    if (currentButtonState != buttonState) {
       buttonState = currentButtonState;
-      switchCount++;
-      if(switchCount % 2 == 0){
-        i = switchCount / 2; 
-        if(i==10){
-          switchCount = 0;
-          i = 0;     
-        }
-        Serial.println(i);      
-      }      
-    }    
-  } 
-   
+      return 1;
+    }
+  }
+  return 0;
 }
+
+void calculateCount(){
+  switchCount++;
+      if (switchCount % 2 == 0) {
+        i = switchCount / 2;
+        if (i == 10) {
+          switchCount = 0;
+          i = 0;
+        }
+        Serial.println(i);
+      }
+}
+int count = 0;
+void loop() {
+    count += button_release();
+    Serial.println(count);
+}
+
+
 
 ch595.h
 
