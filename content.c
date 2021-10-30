2a81f83d4d56da903b47a60e26b608e74a807d43
Robert Hsu <roberthsu2003@gmail.com>
2021-10-30 10:35:50 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,22 +1,36 @@
+#include "ch595.h"
+
 #define dataPin 2
 #define latchPin 3
 #define clockPin 4
 
-void setNumberCH595(byte n,byte latch,byte data,byte c){
-  byte numbers[10] = {B01111110,B00110000,B01101101,B01111001,B00110011,B01011011,B01011111,B01110000,B01111111,B01111011};
-  digitalWrite(latch,LOW);
-  shiftOut(data,c,LSBFIRST,numbers[n]);
-  digitalWrite(latch,HIGH);
-}
-
 void setup() {
-  pinMode(dataPin, OUTPUT);
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  setNumberCH595(5,latchPin,dataPin,clockPin);
+  
+  setNumberCH595(0,latchPin,dataPin,clockPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+
+
+
+ch595.h
+
+/*
+ * setNumber(顯示的數字,latchPin,dataPin,clockPin)
+*/
+#include <arduino.h>
+
+void setNumberCH595(byte n,byte latch,byte data,byte c){
+  
+  pinMode(data, OUTPUT);
+  pinMode(latch, OUTPUT);
+  pinMode(c, OUTPUT);
+  byte numbers[10] = {B01111110,B00110000,B01101101,B01111001,B00110011,B01011011,B01011111,B01110000,B01111111,B01111011};
+  digitalWrite(latch,LOW);
+  shiftOut(data,c,LSBFIRST,numbers[n]);
+  digitalWrite(latch,HIGH);
 }
