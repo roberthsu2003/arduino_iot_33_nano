98be3f2ce44e3bd81524527de201169ba2381da6
Robert Hsu <roberthsu2003@gmail.com>
2021-11-06 15:42:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,4 +1,5 @@
 #include "releaseButton.h"
+#include "sr104.h"
 #define BUTTON 11
 #define TRIG_PIN 3
 #define ECHO_PIN 4
@@ -41,10 +42,19 @@ void buttonClose(){
   } 
 }
 
+
+
+sr104.h
+
+#ifndef __SR104_H__
+#define __SR104_H__
+#define USECOND 58
+
+#include <arduino.h>
+
 int getDistanceCM(byte trigPin, byte echoPin){
   pinMode(trigPin,OUTPUT);
-  pinMode(echoPin,INPUT);
-  const int USECOND = 58;
+  pinMode(echoPin,INPUT);  
   digitalWrite(trigPin,HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin,LOW);
@@ -52,3 +62,32 @@ int getDistanceCM(byte trigPin, byte echoPin){
   int distance = backTime / USECOND;
   return distance;
 }
+
+#endif
+
+
+releaseButton.h
+#ifndef __RELEASEBUTTON_H__
+#define __RELEASEBUTTON_H__
+
+
+int button_release(int buttonPin) { //傳出按鈕改變的次數
+  pinMode(buttonPin, INPUT_PULLUP);
+  static bool buttonState = digitalRead(buttonPin);
+  bool currentButtonState = digitalRead(buttonPin);
+  if (currentButtonState != buttonState) {
+    delay(50); //解決彈跳
+    if (currentButtonState != buttonState) {
+      buttonState = currentButtonState;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int displayNum(int stateCount,int maxNum){ //stateCount是按鈕狀態改變的次數,maxNum是0~maxNum
+    int i = stateCount / 2; 
+    return i % (maxNum+1);    
+}
+
+#endif
