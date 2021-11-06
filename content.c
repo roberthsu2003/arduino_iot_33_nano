dd09afca0ff302f805abc24c3cfba1274f5eee7d
Robert Hsu <roberthsu2003@gmail.com>
2021-11-06 14:14:20 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,6 +1,7 @@
 #include "releaseButton.h"
 #define BUTTON 11
 unsigned int stateChangeCount = 0;
+bool runOnce = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -8,12 +9,27 @@ void setup() {
 
 void loop() {
   stateChangeCount += button_release(BUTTON);  
-  bool switchState = displayNum(stateChangeCount,1);
+  bool switchState = displayNum(stateChangeCount,1);//得到0或1,0為關閉,1為開啟
   if(switchState){
     //開啟
-    Serial.println("開啟");
+    buttonOpen();
   }else{
     //關閉
+    buttonClose();
+  }
+}
+
+void buttonOpen(){
+  if(runOnce == false){ //只會執行一次
+    runOnce = true;
+    Serial.println("Open");
+  }
+}
+
+void buttonClose(){
+  if(runOnce == true){ //只會執行一次
+    runOnce = false;
     Serial.println("關閉");
   }
+ 
 }
