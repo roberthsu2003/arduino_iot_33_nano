4118cddaac6835f5a7e50d8c6bcbd39cc8c63cbd
Robert Hsu <roberthsu2003@gmail.com>
2021-11-06 16:08:57 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,21 +1,29 @@
 #include "releaseButton.h"
 #include "sr104.h"
+#include <BlynkSimpleWiFiNINA.h>
 #define BUTTON 11
 #define TRIG_PIN 3
 #define ECHO_PIN 4
 
 
+
 unsigned int stateChangeCount = 0;
 bool runOnce = false;
+BlynkTimer timer;
+int timerid;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(BUTTON,INPUT_PULLUP);
   pinMode(TRIG_PIN,OUTPUT);
   pinMode(ECHO_PIN,INPUT);
+  timerid = timer.setInterval(1000,oneSecond);
+  timer.disable(timerid);
 }
 
 void loop() {
+  timer.run();
   stateChangeCount += button_release(BUTTON);  
   bool switchState = displayNum(stateChangeCount,1);//得到0或1,0為關閉,1為開啟
   if(switchState){
@@ -29,65 +37,22 @@ void loop() {
 
 void buttonOpen(){
   if(runOnce == false){ //只會執行一次
-    runOnce = true;
-    int distance = getDistanceCM(TRIG_PIN,ECHO_PIN);
-    Serial.println(distance);
+    runOnce = true;   
+    timer.enable(timerid);
+    Serial.println("開啟");  
   }
 }
 
 void buttonClose(){
   if(runOnce == true){ //只會執行一次
     runOnce = false;
+    timer.disable(timerid);
     Serial.println("關閉");
   } 
 }
 
-
-
-sr104.h
-
-#ifndef __SR104_H__
-#define __SR104_H__
-#define USECOND 58
-
-#include <arduino.h>
-
-int getDistanceCM(byte trigPin, byte echoPin){
-  pinMode(trigPin,OUTPUT);
-  pinMode(echoPin,INPUT);  
-  digitalWrite(trigPin,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin,LOW);
-  unsigned long backTime = pulseIn(echoPin,HIGH);
-  int distance = backTime / USECOND;
-  return distance;
+void oneSecond(){
+   int distance = getDistanceCM(TRIG_PIN,ECHO_PIN);
+   Serial.println(distance);
 }
 
-#endif
-
-
-releaseButton.h
-#ifndef __RELEASEBUTTON_H__
-#define __RELEASEBUTTON_H__
-
-
-int button_release(int buttonPin) { //傳出按鈕改變的次數
-  pinMode(buttonPin, INPUT_PULLUP);
-  static bool buttonState = digitalRead(buttonPin);
-  bool currentButtonState = digitalRead(buttonPin);
-  if (currentButtonState != buttonState) {
-    delay(50); //解決彈跳
-    if (currentButtonState != buttonState) {
-      buttonState = currentButtonState;
-      return 1;
-    }
-  }
-  return 0;
-}
-
-int displayNum(int stateCount,int maxNum){ //stateCount是按鈕狀態改變的次數,maxNum是0~maxNum
-    int i = stateCount / 2; 
-    return i % (maxNum+1);    
-}
-
-#endif
