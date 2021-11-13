74f5cd0b1120406bd9ce180a62f88cab12efe700
Robert Hsu <roberthsu2003@gmail.com>
2021-11-13 11:02:55 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,12 +1,13 @@
 #include "releaseButton.h"
 #define BUTTON 5
+#define BUZZER 4
 
 unsigned int stateChangeCount = 0;
 bool isOpen = false;
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON,INPUT_PULLUP);
-
+  pinMode(BUZZER,OUTPUT);
 }
 
 void loop() {
@@ -26,6 +27,9 @@ void buttonOpen(){
   if(isOpen == false){ //只會執行一次
     isOpen = true;
     Serial.println("開");
+    digitalWrite(BUZZER,true);
+    delay(1000);
+    digitalWrite(BUZZER,false);
   }  
 }
 
@@ -33,5 +37,8 @@ void buttonClose(){
   if(isOpen == true){
     isOpen = false;
     Serial.println("關");
+    digitalWrite(BUZZER,true);
+    delay(1000);
+    digitalWrite(BUZZER,false);
   }  
 }
