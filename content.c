5aca0401a1a9f3fd50066c556ba94ccfedf57fb6
Robert Hsu <roberthsu2003@gmail.com>
2021-11-20 16:13:44 +0800

    Update content.c

--- content.c
+++ content.c
@@ -4,6 +4,7 @@
  */
 #include <SPI.h>
 #include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
 
 
 #define RST_PIN         9          
@@ -11,7 +12,7 @@
 
 
 MFRC522 mfrc522;   // 建立MFRC522實體
-
+LiquidCrystal_I2C lcd(0x27,20,2);
 
 void setup() {
   while(!Serial);
@@ -21,10 +22,16 @@ void setup() {
   
 
   mfrc522.PCD_Init(SS_PIN, RST_PIN); // 初始化MFRC522卡
-  Serial.print(F("Reader "));
-  Serial.print(F(": "));
+  Serial.print("Reader ");
+  Serial.print(": ");
   mfrc522.PCD_DumpVersionToSerial(); // 顯示讀卡設備的版本
-  
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(5,0);
+  lcd.print("CLOSE!!");
+  lcd.setCursor(2,1);
+  lcd.print("Hollo! Arduino!");  
 }
 
 
@@ -33,10 +40,10 @@ void loop() {
   // 檢查是不是一張新的卡
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
       // 顯示卡片內容
-      Serial.print(F("Card UID:"));
-      dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size); // 顯示卡片的UID
+      Serial.print("Card UID:");
+      dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size); // 顯示卡片的UID     
       Serial.println();
-      Serial.print(F("PICC type: "));
+      Serial.print("PICC type: ");
       MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
       Serial.println(mfrc522.PICC_GetTypeName(piccType));  //顯示卡片的類型
       mfrc522.PICC_HaltA();  // 卡片進入停止模式
@@ -46,9 +53,14 @@ void loop() {
 /**
  * 這個副程式把讀取到的UID，用16進位顯示出來
  */
-void dump_byte_array(byte *buffer, byte bufferSize) {
-  
-  for (byte i = 0; i < bufferSize; i++) {
+void dump_byte_array(byte buffer[], byte bufferSize) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("CARD_ID:");
+  lcd.setCursor(0,1);
+  for (byte i = 0; i < bufferSize; i++) {    
+    lcd.print(buffer[i] < 0x10 ? " 0" : " ");
+    lcd.print(buffer[i],HEX);
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);        
    }
