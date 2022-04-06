c4089a231b8c3db13f2bbbabb6f79683fafebbf8
徐國堂 <roberthsu2003@gmail.com>
2022-04-06 12:01:45 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,13 +1,16 @@
-
 // C++ code
 //
 void setup()
 {
   Serial.begin(9600);
+  pinMode(13,OUTPUT);
 }
 
 void loop()
 {
   Serial.println("Hello! Arduino");
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
   delay(1000);
 }
