200930565ad346e57ac507bd53cafd0a99d573da
徐國堂 <roberthsu2003@gmail.com>
2022-04-06 14:36:47 +0800

    Update content.c

--- content.c
+++ content.c
@@ -2,16 +2,23 @@
 //
 
 #define BTN 12
+#define LED 13
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(BTN,INPUT);
+  pinMode(LED,OUTPUT);
 }
 
 void loop()
 {
   bool value = digitalRead(BTN);
   Serial.println(value);
+  if(value){
+    digitalWrite(LED,HIGH);
+  }else{
+    digitalWrite(LED,LOW);
+  }
   delay(100);
 }
