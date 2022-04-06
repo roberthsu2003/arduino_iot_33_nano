ab1c18065c38cb3c93c642a2b146bf2a8dbc9b4e
徐國堂 <roberthsu2003@gmail.com>
2022-04-06 14:23:04 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,17 @@
 // C++ code
 //
-#define LED 8
+
+#define BTN 12
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(LED,OUTPUT);
+  pinMode(BTN,INPUT);
 }
 
 void loop()
 {
-  Serial.println("Hello! Arduino");
-  digitalWrite(LED,HIGH);
-  delay(1000);
-  digitalWrite(LED,LOW);
-  delay(1000);
+  bool value = digitalRead(BTN);
+  Serial.println(value);
+  delay(100);
 }
