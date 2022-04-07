27c5e666ea9d216710490730753826627c5e3ea5
徐國堂 <roberthsu2003@gmail.com>
2022-04-07 11:12:53 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,18 +1,19 @@
-// C++ code
-//
+#define LIGHTS 36
 
-#define LIGHT A2
-
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
 }
 
-void loop()
-{
-  
-  int value = analogRead(LIGHT);
-  Serial.println(value);
+void loop() {
+  // put your main code here, to run repeatedly:
+  int lightsValue = analogRead(LIGHTS);
+  byte percentValue = map(lightsValue,1400,4095,0,100);
+  Serial.println(percentValue);
+  if(percentValue >= 40){
+    Serial.println("亮");
+  }else{
+    Serial.println("暗");
+  }
   delay(100);
-  
 }
