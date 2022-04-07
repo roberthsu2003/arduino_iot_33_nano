7c57870af601899aa1c62ee57002e98512fa5cf8
徐國堂 <roberthsu2003@gmail.com>
2022-04-07 11:28:22 +0800

    Update content.c

--- content.c
+++ content.c
@@ -1,8 +1,10 @@
 #define LIGHTS 36
+#define LED 23
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
+pinMode(LED,OUTPUT);
 }
 
 void loop() {
@@ -12,8 +14,10 @@ void loop() {
   Serial.println(percentValue);
   if(percentValue >= 40){
     Serial.println("亮");
+    digitalWrite(LED,LOW);
   }else{
     Serial.println("暗");
+    digitalWrite(LED,HIGH);
   }
   delay(100);
 }
