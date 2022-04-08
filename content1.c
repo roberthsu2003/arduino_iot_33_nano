6dbe123db6f853920350f9a633ffc786680fff78
徐國堂 <roberthsu2003@gmail.com>
2022-04-08 15:05:30 +0800

    Update content1.c

--- content1.c
+++ content1.c
@@ -1,19 +1,29 @@
+data.h
+
+#define BLYNK_TEMPLATE_ID "TMPLPT3"
+#define BLYNK_DEVICE_NAME "Lig"
+#define BLYNK_AUTH_TOKEN "8fMU"
+#define ID "Robert_"
+#define PASS "092"
+
+
+ino
+#define BLYNK_PRINT Serial
+#include "data.h"
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
-#define BLYNK_PRINT Serial
+
 
 /* Fill-in your Template ID (only if using Blynk.Cloud) */
 
-#define BLYNK_TEMPLATE_ID "TMPLPT3cTHDx"
-#define BLYNK_DEVICE_NAME "Lights"
-#define BLYNK_AUTH_TOKEN "8ClqPylXAfjEPAUBWpZu0Eb3pIB8bfMU"
+
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "Robert_iPhone";
-char pass[] = "0926656000";
 char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = ID;
+char pass[] = PASS;
 
 BlynkTimer timer;
 
@@ -34,6 +44,8 @@ void loop()
 
 void myTimerEvent() {
     Serial.println("Hello!");  
-    //Blynk.virtualWrite(V0,rValue);   
+    Blynk.virtualWrite(V0,50);   
     
 }
+
+
