e068283554e42065988927282621ad5f6f47d2ad
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-01 14:27:57 +0800

    Update sketch_jun01b.ino

--- 國發會/上課範例/sketch_jun01b/sketch_jun01b.ino
+++ 國發會/上課範例/sketch_jun01b/sketch_jun01b.ino
@@ -1,11 +1,12 @@
 #define BTN 8
+#define BUZZER 4
 
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(BTN, INPUT_PULLUP);
- 
+  pinMode(BUZZER, OUTPUT);
 }
 
 void loop()
@@ -13,6 +14,13 @@ void loop()
   bool btnValue = digitalRead(BTN);
   if(btnValue==0){
     Serial.println("press");
+    sound();
   }
   delay(100);
+}
+
+void sound(){
+  digitalWrite(BUZZER,HIGH);
+  delay(100);
+  digitalWrite(BUZZER,LOW);
 }
