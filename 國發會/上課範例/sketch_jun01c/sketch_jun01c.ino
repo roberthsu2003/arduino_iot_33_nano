b9185416b83cb26f0f6a3676e49c14da9f092343
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-02 10:06:01 +0800

    Update sketch_jun01c.ino

--- 國發會/上課範例/sketch_jun01c/sketch_jun01c.ino
+++ 國發會/上課範例/sketch_jun01c/sketch_jun01c.ino
@@ -1,13 +1,18 @@
 // C++ code
 //
+#define RED_PIN 5
+
 void setup()
 {
   Serial.begin(9600);
+  pinMode(RED_PIN, OUTPUT);
 }
 
 void loop()
 {
   int value = analogRead(A1);
-  int mapValue = map(value,0,1023,0,255);
+  int mapValue = map(value,0,1023,0,120);
   Serial.println(mapValue);
+  analogWrite(RED_PIN,mapValue);
+  
 }
